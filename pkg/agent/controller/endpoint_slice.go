2a89ad26e914306cb544ee0c96387e3a53277cd9
wangyd1988 <wang_yudong@inspur.com>
2023-09-15 11:10:22 +0800

    for  临时测试

--- pkg/agent/controller/endpoint_slice.go
+++ pkg/agent/controller/endpoint_slice.go
@@ -21,8 +21,6 @@ package controller
 import (
 	"context"
 	"fmt"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/apimachinery/pkg/util/version"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -54,6 +52,7 @@ func newEndpointSliceController(spec *AgentSpecification, syncerConfig broker.Sy
 		conflictCheckWorkQueue: workqueue.New("ConflictChecker"),
 	}
 
+	/*
 	sysncerErr := broker.CreateBrokerClientVersion(&syncerConfig)
 	// 小于v1.21是v1beta1
 	var lessThanversion121 bool = false
@@ -67,7 +66,12 @@ func newEndpointSliceController(spec *AgentSpecification, syncerConfig broker.Sy
 			lessThanversion121 = true
 			brokerResourceType = &discoveryv1beta1.EndpointSlice{}
 		}
-	}
+	}*/
+
+	//for 临时测试修改，临时测试结束后，要删除
+	var brokerResourceType runtime.Object
+	brokerResourceType = &discoveryv1beta1.EndpointSlice{}
+	var lessThanversion121 = true
 
 	syncerConfig.LocalNamespace = metav1.NamespaceAll
 	syncerConfig.LocalClusterID = spec.ClusterID
